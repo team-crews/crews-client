6bc8e135259083a758d7df08315bed9c126a7b5c
송경호 <s07019@naver.com>
2024-08-14 15:32:58 +0900

    feat: 필요 페이지 및 관련 설명 추가 related to #4

---
+++ src/app/recruit/[recruitment-id]/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+  return (
+    <div>
+      ToDO * 모집자(동아리장)이 본인의 모집공고를 관리하는 페이지 * 본인의 모집
+      공고가 아닌 recruitment-id url로 들어오지 못하게 막기 * 서버에 현재 모집
+      공고의 상태(모집전,모집중,평가)를 받아 상태에 따라 화면 출력
+    </div>
+  );
+};
+
+export default Page;
