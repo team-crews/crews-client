f5f6f37fe3e21e963a001cf3b019ea2c4def4c1e
송경호 <s07019@naver.com>
2024-08-14 15:33:49 +0900

    feat: 라우팅 구조 확립 및 인가 처러 구조 확립 related to #4

---
+++ src/router.tsx
@@ -0,0 +1,31 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+
+import RequireAuth from './components/require-auth.tsx';
+import RootLayout from './app/layout.tsx';
+import HomePage from './app/page.tsx';
+import RecruitmentPage from './app/recruitment/[recruitmentId]/page.tsx';
+import RecruitPage from './app/recruit/[recruitment-id]/page.tsx';
+import ApplyPage from './app/apply/[recruitment-id]/page.tsx';
+
+const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<RootLayout />}>
+      <Route path="/" element={<HomePage />} />
+      <Route path="recruitment/:recruitment-id" element={<RecruitmentPage />} />
+
+      <Route element={<RequireAuth availableRoles={['recruiter']} />}>
+        <Route path="recruit/:recruitment-id" element={<RecruitPage />} />
+      </Route>
+
+      <Route element={<RequireAuth availableRoles={['applicant']} />}>
+        <Route path="apply/:recruitment-id" element={<ApplyPage />} />
+      </Route>
+    </Route>,
+  ),
+);
+
+export default router;
