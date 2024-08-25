d79803a220fec5a88720cdd9bef213367b7ca91e
송경호 <s07019@naver.com>
2024-08-26 01:09:22 +0900

    fix: app router directories due to the router change related to #17

--- src/router.tsx
+++ src/router.tsx
@@ -7,8 +7,8 @@ import {
 import RequireAuth from './components/wrapper/require-auth.tsx';
 import RootLayout from './app/layout.tsx';
 import HomePage from './app/page.tsx';
-import RecruitmentPage from './app/recruitment/[recruitmentId]/page.tsx';
-import ApplyPage from './app/apply/[recruitment-id]/page.tsx';
+import RecruitmentPage from './app/recruitment/[recruitment-code]/page.tsx';
+import ApplyPage from './app/apply/[recruitment-code]/page.tsx';
 import RecruitPage from './app/recruit/[recruitment-id]/page.tsx';
 
 const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter(
