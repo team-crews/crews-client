3d55d128c9a301836f843b6944f2a3451c37a8de
송경호 <s07019@naver.com>
2024-08-26 17:27:38 +0900

    feat: add uuid checker function related to #17

---
+++ src/lib/utils/regex.ts
@@ -0,0 +1,7 @@
+function isUUID(targetString: string) {
+  const uuidRegex =
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+  return uuidRegex.test(targetString);
+}
+
+export { isUUID };
