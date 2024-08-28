50830b03ba639ace65bc47e338bd6b970f0b47ff
송경호 <s07019@naver.com>
2024-08-28 17:00:46 +0900

    refactor: change IRole to match with server related to #17

--- src/router.tsx
+++ src/router.tsx
@@ -10,6 +10,7 @@ import HomePage from './app/page.tsx';
 import RecruitmentPage from './app/recruitment/[recruitment-code]/page.tsx';
 import ApplyPage from './app/apply/[recruitment-code]/page.tsx';
 import RecruitPage from './app/recruit/[recruitment-id]/page.tsx';
+import PersistLogin from './components/wrapper/persist-login.tsx';
 
 const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter(
   createRoutesFromElements(
@@ -21,12 +22,14 @@ const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter(
         element={<RecruitmentPage />}
       />
 
-      <Route element={<RequireAuth availableRoles={['applicant']} />}>
-        <Route path="apply/:recruitment-code" element={<ApplyPage />} />
-      </Route>
+      <Route element={<PersistLogin />}>
+        <Route element={<RequireAuth availableRoles={['APPLICANT']} />}>
+          <Route path="apply/:recruitment-code" element={<ApplyPage />} />
+        </Route>
 
-      <Route element={<RequireAuth availableRoles={['recruiter']} />}>
-        <Route path="recruit" element={<RecruitPage />} />
+        <Route element={<RequireAuth availableRoles={['ADMIN']} />}>
+          <Route path="recruit" element={<RecruitPage />} />
+        </Route>
       </Route>
     </Route>,
   ),
