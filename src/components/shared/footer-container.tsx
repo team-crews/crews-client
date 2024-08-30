7ab768db2573732e2be676cc116f0df24fb3e2dc
송경호 <s07019@naver.com>
2024-08-31 01:00:27 +0900

    feat: implement auth header and footer related to #20

--- src/components/shared/footer-container.tsx
+++ src/components/shared/footer-container.tsx
@@ -1,12 +1,18 @@
-import { cn } from "../../lib/utils";
+import { cn } from '../../lib/utils';
+import React from 'react';
 
 interface FooterContainerProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
 }
 
-const FooterContainer = ({ children,className }: FooterContainerProps) => {
+const FooterContainer = ({ children, className }: FooterContainerProps) => {
   return (
-    <div className={cn("fixed bottom-0 left-0 w-screen border-t border-crews-b02 bg-crews-w01 px-[1.25rem] py-[0.625rem]", className)}>
+    <div
+      className={cn(
+        'fixed bottom-0 left-0 flex w-dvw justify-end gap-2 border-t border-crews-b02 bg-crews-w01 px-4 py-2',
+        className,
+      )}
+    >
       {children}
     </div>
   );
--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,7 +1,22 @@
+import AnchorIcon from '../../assets/icons/anchor-icon.svg?react';
+import CircleUserIcon from '../../assets/icons/circle-user-icon.svg?react';
+
 const Header = () => {
   return (
-    <header className="w-full">
-      <h1>Header</h1>
+    <header className="fixed left-0 top-0 flex w-full items-center justify-between bg-crews-b02 px-4 py-2">
+      <div className="flex items-center gap-2 text-crews-b05">
+        <AnchorIcon className="h-6 w-6" />
+        <p className="text-2xl font-semibold">Crews</p>
+      </div>
+      <div className="flex items-center gap-3 text-crews-bk01">
+        <div className="flex items-center gap-2">
+          <CircleUserIcon className="h-6 w-6" />
+          <p className="text-sm font-medium">운영진 | 크루즈 11기</p>
+        </div>
+        <button>
+          <p className="text-xs font-light underline">로그아웃</p>
+        </button>
+      </div>
     </header>
   );
 };
