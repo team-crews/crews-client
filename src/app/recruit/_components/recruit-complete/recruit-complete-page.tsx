23dfd063ce9d7268478523fd5495ed3164e8f17e
송경호 <s07019@naver.com>
2024-08-31 01:02:29 +0900

    feat: implement recruit complete page styling related to #20

---
+++ src/app/recruit/_components/recruit-complete/recruit-complete-page.tsx
@@ -0,0 +1,24 @@
+import Container from '../../../../components/shared/container.tsx';
+import HeaderSection from './header-section.tsx';
+import CompetitionRateSection from './competition-rate-section.tsx';
+import ApplicationOverviewSection from './application-overview-section.tsx';
+import { Button } from '../../../../components/ui/button.tsx';
+import FooterContainer from '../../../../components/shared/footer-container.tsx';
+
+const RecruitCompletePage = () => {
+  return (
+    <Container className="flex h-fit flex-col py-10">
+      <HeaderSection />
+      <div className="my-8 flex flex-col gap-8">
+        <CompetitionRateSection />
+        <ApplicationOverviewSection />
+      </div>
+      <FooterContainer className="flex w-full justify-end">
+        <Button size="lg">임시 저장</Button>
+        <Button size="lg">평가 완료</Button>
+      </FooterContainer>
+    </Container>
+  );
+};
+
+export default RecruitCompletePage;
