3a84f1f49f8c045ead6fc68c41270e975d73043c
kyuhho <rbgh0114@naver.com>
2024-09-01 23:23:40 +0900

    feat: add option selection section exception control logic

--- src/app/recruit/[recruitment-id]/_components/recruit-make/option-section.tsx
+++ src/app/recruit/[recruitment-id]/_components/recruit-make/option-section.tsx
@@ -1,6 +1,6 @@
 import { useFormContext } from 'react-hook-form';
 import Typography from '../../../../../components/shared/typography';
-import WordLimitInput from './word-limit-input';
+
 import { Switch } from '../../../../../components/ui/switch';
 import { QuestionType } from '../../../../../lib/enums';
 
@@ -9,14 +9,48 @@ interface OptionSectionProps {
   questionIndex: number;
 }
 
-// const MAX_SELECTION = 10;
+const MAX_SELECTION_LENGTH = 3;
+
+const MAX_WORD_LIMIT_LENGTH = 4;
 
 const OptionSection = ({ sectionIndex, questionIndex }: OptionSectionProps) => {
-  const { register, watch, setValue } = useFormContext();
+  const { watch, setValue } = useFormContext();
 
   const questionType = watch(
     `sections.${sectionIndex}.questions.${questionIndex}.type`,
   );
+
+  const wordLimitValue = watch(
+    `sections.${sectionIndex}.questions.${questionIndex}.wordLimit`,
+  );
+
+  const minimumSelectionValue = watch(
+    `sections.${sectionIndex}.questions.${questionIndex}.minimumSelection`,
+  );
+
+  const maximumSelectionValue = watch(
+    `sections.${sectionIndex}.questions.${questionIndex}.maximumSelection`,
+  );
+
+  const handleLimitedNumberChange = (
+    name: string,
+    limit: number,
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const value = e.target.value;
+
+    const numberRegex = /^$|^\d+$/;
+
+    if (!numberRegex.test(value) || value.length > limit) {
+      return;
+    }
+
+    const isValueNull = value !== '';
+
+    const updatedValue = isValueNull ? Number(value) : null;
+
+    setValue(name, updatedValue);
+  };
 
   return (
     <section className="mt-[1rem] flex w-full justify-end">
@@ -46,39 +80,63 @@ const OptionSection = ({ sectionIndex, questionIndex }: OptionSectionProps) => {
               <Typography className="text-[0.875rem] text-crews-g06">
                 최소 선택
               </Typography>
-              <input
-                type="number"
-                className="w-[1.5rem] text-[0.875rem] font-bold underline"
-                {...register(
-                  `sections.${sectionIndex}.questions.${questionIndex}.minimumSelection`,
-                )}
-                placeholder="000"
-              />
+              <div className="flex h-[0.875rem] w-[1.75rem] items-center justify-center border-b-[1px] border-crews-bk02">
+                <input
+                  value={minimumSelectionValue}
+                  className="h-full w-full text-[0.875rem] font-bold text-crews-bk02"
+                  placeholder="000"
+                  onChange={(e) =>
+                    handleLimitedNumberChange(
+                      `sections.${sectionIndex}.questions.${questionIndex}.minimumSelection`,
+                      MAX_SELECTION_LENGTH,
+                      e,
+                    )
+                  }
+                />
+              </div>
             </div>
             <div className="flex items-center gap-[0.5rem]">
               <Typography className="text-[0.875rem] text-crews-g06">
                 최대 선택
               </Typography>
-              <input
-                type="number"
-                className="w-[1.5rem] text-[0.875rem] font-bold underline"
-                {...register(
-                  `sections.${sectionIndex}.questions.${questionIndex}.maximumSelection`,
-                )}
-              />
+              <div className="flex h-[0.875rem] w-[1.75rem] items-center justify-center border-b-[1px] border-crews-bk02">
+                <input
+                  value={maximumSelectionValue}
+                  className="h-full w-full text-[0.875rem] font-bold text-crews-bk02"
+                  placeholder="000"
+                  onChange={(e) =>
+                    handleLimitedNumberChange(
+                      `sections.${sectionIndex}.questions.${questionIndex}.maximumSelection`,
+                      MAX_SELECTION_LENGTH,
+                      e,
+                    )
+                  }
+                />
+              </div>
             </div>
           </div>
         )}
 
         {questionType === QuestionType.NARRATIVE && (
-          <div className="flex items-center">
+          <div className="flex items-center gap-[0.25rem]">
             <Typography className="text-[0.875rem] text-crews-g06">
               글자 수 제한
             </Typography>
-            <WordLimitInput
-              sectionIndex={sectionIndex}
-              questionIndex={questionIndex}
-            />
+
+            <div className="flex h-[0.875rem] w-[2.375rem] items-center justify-center border-b-[1px] border-crews-bk02">
+              <input
+                value={wordLimitValue}
+                className="h-full w-full text-[0.875rem] font-bold text-crews-bk02"
+                placeholder="0000"
+                onChange={(e) =>
+                  handleLimitedNumberChange(
+                    `sections.${sectionIndex}.questions.${questionIndex}.wordLimit`,
+                    MAX_WORD_LIMIT_LENGTH,
+                    e,
+                  )
+                }
+              />
+            </div>
 
             <Typography className="text-[0.875rem] text-crews-g06">
               자
