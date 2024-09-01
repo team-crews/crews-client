a5972c8ef812ac3041cac1a3393e22793601a6da
송경호 <s07019@naver.com>
2024-09-01 15:08:15 +0900

    feat: add heading to recruit make page and wrap all recruit page with a common container related to #22

---
+++ src/app/recruit/_components/recruit-make/header-section.tsx
@@ -0,0 +1,25 @@
+import ThumbTackIcon from '../../../../assets/icons/thumbtack-icon.svg?react';
+
+const HeaderSection = () => {
+  return (
+    <section>
+      <div className="flex flex-col gap-3">
+        <div className="flex items-center gap-2 text-crews-bk02">
+          <ThumbTackIcon className="h-5 w-5" />
+          <p className="text-[28px] font-bold">지원서 생성하기</p>
+        </div>
+        <div className="flex flex-col gap-1 text-crews-g05">
+          <p className="text-base font-normal">
+            모집을 위한 지원서를 생성합니다. 하단의 '모집 시작' 버튼을 눌러
+            모집을 시작하세요.
+          </p>
+          <p className="text-base font-normal">
+            추가적인 작업이 필요하다면 꼭 하단의 '임시 저장'을 눌러주세요.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeaderSection;
