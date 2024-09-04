fe3235b439776001f4bc07cdcfbfd78ea1c5e24a
송경호 <s07019@naver.com>
2024-09-04 11:36:25 +0900

    feat: trivial changes for apis and settings related to #30

--- src/app/recruitment/[recruitmentCode]/page.tsx
+++ src/app/recruitment/[recruitmentCode]/page.tsx
@@ -1,8 +1,25 @@
 import Container from '../../../components/shared/container.tsx';
 import ApplyForm from './_components/apply-form.tsx';
 import { Skeleton } from '../../../components/shared/skeleton.tsx';
+import { readRecruitmentByCode } from '../../../apis/base-api.ts';
+import { useQuery } from '@tanstack/react-query';
+import { Navigate, useParams } from 'react-router-dom';
+import Loading from '../../../components/shared/loading.tsx';
+import { printCustomError } from '../../../lib/utils/error.ts';
 
 const Page = () => {
+  const { recruitmentCode } = useParams<{ recruitmentCode: string }>();
+  const readQuery = useQuery({
+    queryKey: ['recruitmentByCode'],
+    queryFn: () => readRecruitmentByCode(recruitmentCode!),
+  });
+
+  if (readQuery.isFetching) return <Loading />;
+  else if (readQuery.isError) {
+    printCustomError(readQuery.error, 'readQuery');
+    return <Navigate to="/error" replace />;
+  }
+  console.log(readQuery.data);
   return (
     <Container className="0 flex gap-10">
       <section className="flex flex-grow items-center justify-center">
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,13 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
