3b8d85453dcbe2700f5cca5c043de926c1cd3a15
송경호 <s07019@naver.com>
2024-09-04 11:26:02 +0900

    feat: shrink size of three header-section related to #30

--- src/app/recruit/_components/recruit-wait/header-section.tsx
+++ src/app/recruit/_components/recruit-wait/header-section.tsx
@@ -6,16 +6,16 @@ const HeaderSection = () => {
       <div className="flex flex-col gap-3">
         <div className="flex items-center gap-2 text-crews-bk02">
           <ThumbTackIcon className="h-5 w-5" />
-          <p className="text-[28px] font-bold">모집 중</p>
+          <p className="text-2xl font-bold">지원서 생성하기</p>
         </div>
         <div className="flex flex-col gap-1 text-crews-g05">
-          <p className="text-base font-normal">
+          <p className="text-base font-light">
             모집 중인 모집 공고를 관리할 수 있습니다. 현재 지원한 지원자의 수를
             확인해 보세요.
           </p>
-          <p className="break-words text-base font-normal">
-            필요 시 마감 기간을 연장하실 수 있지만 일찍 마감은 불가하니
-            주의해주세요. 😔
+          <p className="text-base font-light">
+            필요 시 마감 기간을 연장하실 수 있지만 마감 일정을 당기는 것은
+            불가하니 주의해주세요. 😔
           </p>
         </div>
       </div>
