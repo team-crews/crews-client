c097851519273c6a446f2b8db38c700177c143ce
송경호 <s07019@naver.com>
2024-09-07 15:59:20 +0900

    fix: fix ga settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { VitePluginRadar } from 'vite-plugin-radar';
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [
+    svgr(),
+    react(),
+    VitePluginRadar({
+      analytics: { id: process.env.VITE_GA_ID! },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
