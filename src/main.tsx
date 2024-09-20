083f0e364d1c357ccfeabb9636a392d4e7e0eb27
송경호 <s07019@naver.com>
2024-09-20 16:48:00 +0900

    feat: add sentry settings

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/font.css';
 
-import { RouterProvider } from 'react-router-dom';
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  RouterProvider,
+  useLocation,
+  useNavigationType,
+} from 'react-router-dom';
 import router from './router.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import * as Sentry from '@sentry/react';
+import { useEffect } from 'react';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,10 +24,31 @@ const queryClient = new QueryClient({
   },
 });
 
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+    Sentry.replayIntegration(),
+  ],
+
+  tracesSampleRate: 0.6, // default 는 1이나 너무 민감하므로 실제는 0.6 정도로 낮춥니다.
+
+  tracePropagationTargets: ['localhost', /^https:\/\/join-crews\.site/],
+
+  replaysSessionSampleRate: 0.1, // 리플레이를 샘플링할 비율을 설정합니다. 0.1은 100% 중 10% 정도 기록합니다.
+  replaysOnErrorSampleRate: 1.0, // 오류가 발생했을 때 세션 리플레이를 기록할 확률을 설정합니다.
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
   </QueryClientProvider>,
-  /*</React.StrictMode>,*/
+  // </React.StrictMode>
 );
