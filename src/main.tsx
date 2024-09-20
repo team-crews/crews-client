af5321207a5242a781bd5993bd55ff6185ee1811
송경호 <s07019@naver.com>
2024-09-20 16:53:47 +0900

    feat: add sentry settings

--- src/main.tsx
+++ src/main.tsx
@@ -24,26 +24,28 @@ const queryClient = new QueryClient({
   },
 });
 
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect,
-      useLocation,
-      useNavigationType,
-      createRoutesFromChildren,
-      matchRoutes,
-    }),
-    Sentry.replayIntegration(),
-  ],
+if (import.meta.env.MODE !== 'development') {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+      Sentry.reactRouterV6BrowserTracingIntegration({
+        useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes,
+      }),
+      Sentry.replayIntegration(),
+    ],
 
-  tracesSampleRate: 0.6, // default 는 1이나 너무 민감하므로 실제는 0.6 정도로 낮춥니다.
+    tracesSampleRate: 0.6, // default 는 1이나 너무 민감하므로 실제는 0.6 정도로 낮춥니다.
 
-  tracePropagationTargets: ['localhost', /^https:\/\/join-crews\.site/],
+    tracePropagationTargets: ['localhost', /^https:\/\/join-crews\.site/],
 
-  replaysSessionSampleRate: 0.1, // 리플레이를 샘플링할 비율을 설정합니다. 0.1은 100% 중 10% 정도 기록합니다.
-  replaysOnErrorSampleRate: 1.0, // 오류가 발생했을 때 세션 리플레이를 기록할 확률을 설정합니다.
-});
+    replaysSessionSampleRate: 0.1, // 리플레이를 샘플링할 비율을 설정합니다. 0.1은 100% 중 10% 정도 기록합니다.
+    replaysOnErrorSampleRate: 1.0, // 오류가 발생했을 때 세션 리플레이를 기록할 확률을 설정합니다.
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
