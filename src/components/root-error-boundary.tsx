a289098e1c70717945bfcf568c2006521cccc266
송경호 <s07019@naver.com>
2024-09-28 00:22:13 +0900

    feat: create root error boundary

---
+++ src/components/root-error-boundary.tsx
@@ -0,0 +1,47 @@
+import { useNavigate, useRouteError } from 'react-router-dom';
+import React from 'react';
+import * as Sentry from '@sentry/react';
+import Container from './shared/container.tsx';
+import { getErrorMessage } from '../lib/utils/error.ts';
+import HelpButton from './shared/help-button.tsx';
+import FaceSadTearIcon from '../assets/icons/face-sad-tear-icon.svg?react';
+
+const RootErrorBoundary = () => {
+  const error = useRouteError() as Error;
+
+  React.useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
+  const navigate = useNavigate();
+  return (
+    <>
+      <Container className="bg-red flex items-center justify-center">
+        <div className="max-w-screen-sm text-center text-crews-b07">
+          <FaceSadTearIcon className="mx-auto h-10 w-10" />
+          <p className="mt-4 text-4xl font-semibold">
+            {getErrorMessage(error)}
+          </p>
+
+          <p className="mt-4 text-center text-base font-light text-crews-g05">
+            이용에 불편을 드려 죄송합니다 ( ᴗ_ᴗ̩̩. ) 더 나은 서비스를 제공하기
+            위해 노력하겠습니다.
+            <br />
+            문제가 지속해서 발생할 경우 좌측 하단의 🚨를 눌러 관리자에게
+            문의해주세요.
+          </p>
+
+          <button
+            className="mt-12 rounded-sm bg-crews-b05 px-16 py-2.5 text-sm tracking-widest text-crews-w01"
+            onClick={() => navigate(-1)}
+          >
+            돌아가기
+          </button>
+        </div>
+      </Container>
+      <HelpButton />
+    </>
+  );
+};
+
+export default RootErrorBoundary;
