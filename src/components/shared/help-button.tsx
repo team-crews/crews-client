e658eeee5eb44c5a7a12e077f5b32a4f7efe9d36
송경호 <s07019@naver.com>
2024-10-01 16:39:18 +0900

    feat: change chat button

--- src/components/shared/help-button.tsx
+++ src/components/shared/help-button.tsx
@@ -1,12 +1,17 @@
 const url = import.meta.env.VITE_KAKAO_OPEN_CHAT;
+import ChatIcon from '../../assets/icons/comment-icon.svg?react';
 
 const HelpButton = () => {
   return (
-    <div className="fixed bottom-4 left-4 z-50 flex h-8 w-8 cursor-pointer items-center justify-center overflow-hidden rounded-full bg-crews-r01">
-      <a href={url} className="text-base font-bold" target="_blank">
-        🚨
-      </a>
-    </div>
+    <a
+      href={url}
+      className="fixed bottom-4 left-4 z-50 cursor-pointer"
+      target="_blank"
+    >
+      <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-full bg-crews-b04 text-crews-w01 hover:opacity-80">
+        <ChatIcon className="h-6 w-6" />
+      </div>
+    </a>
   );
 };
 
