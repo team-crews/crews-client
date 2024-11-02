6049df2de587d99e314d9d61942d345f84718636
송경호 <s07019@naver.com>
2024-11-03 00:41:18 +0900

    chore: comment out custom hooks for apply page

--- src/app/apply/[recruitmentCode]/_hooks/use-section-selection.tsx
+++ src/app/apply/[recruitmentCode]/_hooks/use-section-selection.tsx
@@ -1,58 +1,55 @@
-import { UseFormClearErrors } from 'react-hook-form';
-import { useEffect, useState } from 'react';
-import { getInitialSectionSelection } from '../_utils/utils';
-import { SHARED_SECTION_INDEX } from '../page';
-import { z } from 'zod';
-import { RecruitmentSchema } from '../../../../lib/types/schemas/recruitment-schema.ts';
-import { ApplicationDetailSchema } from '../../../../lib/types/schemas/application-schema.ts';
+/*
+  FixMe
+  - 필요없다고 완전히 판단되면 제거
+ */
 
-type UseSectionSelectionParams = {
-  recruitment?: z.infer<typeof RecruitmentSchema>;
-  application?: z.infer<typeof ApplicationDetailSchema>;
+// type UseSectionSelectionParams = {
+//   recruitment?: z.infer<typeof RecruitmentSchema>;
+//   application?: z.infer<typeof ApplicationDetailSchema>;
+//
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   clearErrors: UseFormClearErrors<any>;
+// };
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  clearErrors: UseFormClearErrors<any>;
-};
-
-export const useSectionSelection = ({
-  recruitment,
-  application,
-  clearErrors,
-}: UseSectionSelectionParams) => {
-  // 공통 섹션
-  const sharedSection = recruitment?.sections[SHARED_SECTION_INDEX];
-
-  // 초기 focus 섹션 index
-  const initialSectionSelections = getInitialSectionSelection(
-    application?.sections,
-    recruitment?.sections,
-  );
-
-  // 선택된 섹션 index
-  const [selectedSectionIndex, setSelectedSectionIndex] = useState<number>(
-    initialSectionSelections,
-  );
-
-  // 선택된 섹션
-  const selectedSection = recruitment?.sections[selectedSectionIndex];
-  const isOnlySharedSection = recruitment?.sections.length === 1;
-
-  useEffect(() => {
-    setSelectedSectionIndex(initialSectionSelections);
-  }, [initialSectionSelections]);
-
-  const handleSectionSelectionChange = (index: number) => {
-    setSelectedSectionIndex(index);
-
-    // 선택된 섹션 변경 시, 에러 메시지 초기화
-    clearErrors(`sections.${selectedSectionIndex}.answers`);
-  };
-
-  return {
-    sharedSection,
-    selectedSectionIndex,
-    selectedSection,
-    isOnlySharedSection,
-    handleSectionSelectionChange,
-  };
-};
+// export const useSectionSelection = ({
+//   recruitment,
+//   application,
+//   clearErrors,
+// }: UseSectionSelectionParams) => {
+//   // 공통 섹션
+//   const sharedSection = recruitment?.sections[SHARED_SECTION_INDEX];
+//
+//   // 초기 focus 섹션 index
+//   const initialSectionSelections = getInitialSectionSelection(
+//     application?.sections,
+//     recruitment?.sections,
+//   );
+//
+//   // 선택된 섹션 index
+//   const [selectedSectionIndex, setSelectedSectionIndex] = useState<number>(
+//     initialSectionSelections,
+//   );
+//
+//   // 선택된 섹션
+//   const selectedSection = recruitment?.sections[selectedSectionIndex];
+//   const isOnlySharedSection = recruitment?.sections.length === 1;
+//
+//   useEffect(() => {
+//     setSelectedSectionIndex(initialSectionSelections);
+//   }, [initialSectionSelections]);
+//
+//   const handleSectionSelectionChange = (index: number) => {
+//     setSelectedSectionIndex(index);
+//
+//     // 선택된 섹션 변경 시, 에러 메시지 초기화
+//     clearErrors(`sections.${selectedSectionIndex}.answers`);
+//   };
+//
+//   return {
+//     sharedSection,
+//     selectedSectionIndex,
+//     selectedSection,
+//     isOnlySharedSection,
+//     handleSectionSelectionChange,
+//   };
+// };
