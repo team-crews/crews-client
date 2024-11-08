9a7e4a0cb1b0bc3edbca7d6414907a5faef5ca75
kyuhho <rbgh0114@naver.com>
2024-11-08 14:08:13 +0900

    feat: apply preview feature

--- src/app/recruitment/info/_components/info-section.tsx
+++ src/app/recruitment/info/_components/info-section.tsx
@@ -1,22 +1,18 @@
 import { z } from 'zod';
-import RecruitMetaBox from '../../../../components/recruitment-view/recruit-meta-box';
+
 import SectionBoxes from '../../../../components/recruitment-view/section-boxes';
 import Container from '../../../../components/shared/container';
-import { RecruitmentSchema } from '../../../../lib/types/schemas/recruitment-schema';
+
+import { SectionSchema } from '../../../../lib/types/schemas/section-schema';
 
 interface InfoSectionProps {
-  recruitment: z.infer<typeof RecruitmentSchema>;
+  sections: z.infer<typeof SectionSchema>[];
 }
 
-const InfoSection = ({ recruitment }: InfoSectionProps) => {
+const InfoSection = ({ sections }: InfoSectionProps) => {
   return (
-    <Container className="flex w-[40rem] flex-col gap-8 py-4">
-      <RecruitMetaBox
-        title={recruitment.title}
-        description={recruitment.description}
-        deadline={recruitment.deadline}
-      />
-      <SectionBoxes sections={recruitment.sections} />
+    <Container className="flex w-[44rem] flex-col gap-8 py-4">
+      <SectionBoxes sections={sections} />
     </Container>
   );
 };
--- src/app/recruitment/info/page.tsx
+++ src/app/recruitment/info/page.tsx
@@ -8,8 +8,15 @@ import { Button } from '../../../components/shadcn/button';
 
 import InfoSection from './_components/info-section';
 
+import ViewSection from './_components/view-section';
+import CrewsFooter from '../../../components/molecule/crews-footer';
+
+import useDialog from '../../../hooks/use-dialog';
+import PreviewDialog from './_components/preview-dialog';
+
 const Page = () => {
   const navigate = useNavigate();
+  const dialogReturns = useDialog();
 
   const [searchParams] = useSearchParams();
   const title = searchParams.get('title');
@@ -19,11 +26,36 @@ const Page = () => {
     queryFn: () => readRecruitmentSearchBy(title!),
   });
 
+  const handlePreviewClick = () => {
+    dialogReturns.toggleOpen();
+  };
+
+  const handleApplyClick = () => {
+    navigate('/apply/' + readQuery.data?.code);
+  };
+
   if (readQuery.isFetching || readQuery.isPending) return <Loading />;
   else if (readQuery.isSuccess) {
     return (
-      <Container className="flex h-auto w-full justify-center overflow-scroll px-4 pb-8 pt-16">
-        <InfoSection recruitment={readQuery.data} />
+      <Container className="flex h-auto w-full justify-center gap-4 overflow-scroll px-4 pb-8 pt-16">
+        <ViewSection
+          title={readQuery.data.title}
+          description={readQuery.data.description}
+          deadline={readQuery.data.deadline}
+        />
+
+        <CrewsFooter>
+          <Button onClick={handlePreviewClick} size="lg" variant="black">
+            지원서 미리보기
+          </Button>
+          <Button onClick={handleApplyClick} size="lg">
+            지원하기
+          </Button>
+        </CrewsFooter>
+
+        <PreviewDialog {...dialogReturns}>
+          <InfoSection sections={readQuery.data.sections} />
+        </PreviewDialog>
       </Container>
     );
   } else if (printCustomError(readQuery.error, 'readQuery') === 404) {
